1821dc6774a57c39991408aca36c5615f13ca2be
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-01-20 14:52:31 +0200

    Delete Quotation.java

--- src/Quotation.java
+++
@@ -1,9 +0,0 @@
-
-public class Quotation {
-
-    String quote = "Be a first rate version of yourself, not a second rate version of someone else";
-
-    public void display() {
-        System.out.println(quote);
-    }
-}
