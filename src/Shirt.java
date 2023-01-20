91d7c0dacfea51dca74d914e420b9a07b4448ba8
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-01-20 14:51:44 +0200

    Add files via upload

---
+++ src/Shirt.java
@@ -0,0 +1,19 @@
+
+public class Shirt {
+
+    public int shirtID = 451; // стандартне значення номера моделі сорочки
+    public String description = "Soft cotton fabric provides low creasing and wearing comfort. Suitable for different styles of clothing."; // стандартний опис сорочки
+    // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
+    public char colorCode = 'U';
+    public double price = 225.0; // стандартна вартість сорочки
+    public int quantityInStock = 0; // стандартна кількість на складі
+
+    // цей метод просто виводить всю інформацію про сорочку на екран
+    public void displayShirtInformation() {
+        System.out.println("Shirt ID: " + shirtID);
+        System.out.println("Shirt description:" + description);
+        System.out.println("Color Code: " + colorCode);
+        System.out.println("Shirt price: $" + price);
+        System.out.println("Quantity in stock: " + quantityInStock);
+    } // кінець методу displayShirtInformation
+} // кінець опису класу
