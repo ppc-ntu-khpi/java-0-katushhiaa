bb0460c8442e734d2a988e17cc52d225a860bcb3
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-01-20 14:51:28 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,9 @@
+
+public class Quotation {
+
+    String quote = "Be a first rate version of yourself, not a second rate version of someone else";
+
+    public void display() {
+        System.out.println(quote);
+    }
+}
